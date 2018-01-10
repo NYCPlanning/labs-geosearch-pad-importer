2fb232988f5861b38c7072b6679682fe5e75cf7f
allthesignals <wilburnforce@gmail.com>
2018-01-10 16:06:00 -0500

    Script successfully coerces mixed house number values into integers, creates odd-number sequences, and unnests those sequence lists into new rows that represent the expanded dataset.

--- munge.R
+++ munge.R
@@ -1,9 +1,84 @@
 library(downloader)
+library(gdata)
+library(tidyr)
+library(readr)
+library(stringr)
+library(dplyr)
+
 source <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/pad17d.zip"
 
-# parity field -> evens & odds
+# `parity` field
+# 0 - NAP (no address range)
+# 1 - Defining address range consists of odd house numbers
+# 2 - Defining address range consists of even house numbers
+parityLookup <- c(
+  c(0,'no address range'),c(1,'odd'),c(2,'even')
+)
+
+addrtypeLookup <-c(
+  c("blank", "Real Address Range"),
+  c("B", "NAUB"),
+  c("F", "Vacant Street Frontage"),
+  c("G", "NAP of Complex"),
+  c("N", "NAP of Simplex"),
+  c("Q", "Pseudo-Address Range"),
+  c("R", "Real Street of Vanity Address"),
+  c("V", "Vanity Address"),
+  c("W", "Blank-Wall Building Façade"),
+  c("X", "NAP of Constituent Entity of Complex")
+)
 
 download(source, dest="data/dataset.zip", mode="wb") 
 unzip ("data/dataset.zip", exdir = "./data")
 
-pad <- read.csv('data/bobaadr.txt')
+pad <- read.csv('data/bobaadr.txt', stringsAsFactors=FALSE)
+
+pad$bbl <- paste(
+  str_pad(
+    as.character(pad$boro), 5, pad="0"
+  ),
+  str_pad(
+    as.character(pad$block), 3, pad="0"
+  ),
+  str_pad(
+    as.character(pad$lot), 3, pad="0"
+  ),
+  sep=""
+)
+
+# trim whitespace
+pad$lhnd <- trim(
+  pad$lhnd
+)
+
+pad$hhnd <- trim(
+  pad$hhnd
+)
+
+# - Parse house numbers into integers
+pad$lnumber <- parse_number(pad$lhnd)
+pad$rnumber <- parse_number(pad$hhnd)
+
+# - Count of every odd or even house number based on parity
+pad$difference <- (pad$rnumber - pad$lnumber)
+
+# - The difference of any pair of odd or even numbers will always be even. 
+pad$interpolatedCount <- ((pad$difference / 2) - 1)
+
+# - Assume addition of two 
+pad$finalCount <- pad$interpolatedCount + 2
+
+# - subet only those we need
+expandingAddresses <- pad[which(pad$finalCount > 1),]
+
+expandingAddresses$houseNums <-
+  apply(
+    expandingAddresses,
+    1,
+    function(x) {
+      paste(seq(x['lnumber'], x['rnumber'], 2), collapse=',')
+    })
+
+finalTable <- expandingAddresses %>% 
+  transform(houseNums = strsplit(houseNums, ',')) %>%
+  unnest(houseNums)
