7cdc95eeffb638c1f3826089e4deb4eb7cd78bf7
allthesignals <wilburnforce@gmail.com>
2018-01-10 12:05:54 -0500

    Script downloads and decompresses PAD data; Update gitignore;

---
+++ munge.R
@@ -0,0 +1,9 @@
+library(downloader)
+source <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/pad17d.zip"
+
+# parity field -> evens & odds
+
+download(source, dest="data/dataset.zip", mode="wb") 
+unzip ("data/dataset.zip", exdir = "./data")
+
+pad <- read.csv('bobaadr.txt')
