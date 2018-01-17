a520b64507b8c9d1d2ebd6b862e2c11b5b73614a
allthesignals <wilburnforce@gmail.com>
2018-01-17 16:20:31 -0500

    Zip differently...

--- push-to-bucket.sh
+++ push-to-bucket.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 Rscript ./munge.R
 
-zip data/labs-geosearch-pad-normalized.zip data/labs-geosearch-pad-normalized.csv 
-zip data/labs-geosearch-pad-normalized-sample-lg.zip data/labs-geosearch-pad-normalized-sample-lg.csv
-zip data/labs-geosearch-pad-normalized-sample-md.zip data/labs-geosearch-pad-normalized-sample-md.csv
-zip data/labs-geosearch-pad-normalized-sample-sm.zip data/labs-geosearch-pad-normalized-sample-sm.csv
+cd data
+zip labs-geosearch-pad-normalized.zip labs-geosearch-pad-normalized.csv 
+zip labs-geosearch-pad-normalized-sample-lg.zip labs-geosearch-pad-normalized-sample-lg.csv
+zip labs-geosearch-pad-normalized-sample-md.zip labs-geosearch-pad-normalized-sample-md.csv
+zip labs-geosearch-pad-normalized-sample-sm.zip labs-geosearch-pad-normalized-sample-sm.csv
+cd ..
 
 s3cmd put data/labs-geosearch-pad-normalized*.zip s3://planninglabs/geosearch-data/
 s3cmd setacl s3://planninglabs/geosearch-data/ --acl-public --recursive
