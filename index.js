d044de6e7562d523005703334d6621f6f11d8a80
Chris Whong <chris@whong.org>
2018-01-17 23:51:07 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const peliasConfig = require('pelias-config').generate(require('./schema'));
-const _ = require('lodash');
-const logger = require('pelias-logger', {
-  level: 'verbose'
-}).get('nycpad');
-
 const importPipeline = require('./stream/importPipeline');
 
 importPipeline.import();
