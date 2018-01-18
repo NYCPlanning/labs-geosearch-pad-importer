a00347098341d89a9ac64b169234b666e1e60afb
jtalati <jtalati@planning.nyc.gov>
2018-01-18 17:09:20 -0500

    added hyphenNoSuffix type code addresses Issues#10

--- munge.R
+++ munge.R
@@ -65,7 +65,8 @@ pad <- pad %>%
       lhns == hhns                                                                          ~ 'singleAddress',
       addrtype == 'G' | addrtype == 'N' | addrtype == 'X'                                   ~ 'nonAddressable',
       grepl("^0", lhns) & grepl("^0", hhns) & grepl("000AA$", lhns) & grepl("000AA$", hhns) ~ 'numericType',
-      str_sub(lhns, 1, 1) == "1" & lhnd != hhnd & str_sub(lhns, 10, 11) == "AA"             ~ 'hyphenNoSuffix'
+      str_sub(lhns, 1, 1) == "1" & lhnd != hhnd & str_sub(lhns, 10, 11) == "AA"             ~ 'hyphenNoSuffix',
+      str_sub(lhns, 1, 1) == "0" & str_sub(lhns, 10, 10) %in% c("M","N","O")                ~ 'nohyphenSuffix'
       # as.numeric(str_sub(lhns, 7, 9)) > 0 & str_sub(lhns, 10, 11) == "AA" & !is.na(lhns)  ~ 'nonNumericDashSepNoSuffix'
     )
   )
